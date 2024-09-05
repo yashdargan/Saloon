2321724c3a3a58b51eeaa7737f2a54215ab0ca0f
yashdargan <yashrajdargan@gmail.com>
2024-09-05 11:32:50 +0530

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all source files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

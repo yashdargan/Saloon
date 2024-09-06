7301f5efbcc0b93445120c85072f908aa96b4f62
yashdargan <yashrajdargan@gmail.com>
2024-09-06 22:58:15 +0530

    feat: minor adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ function App() {
 
     <>
       <Navbar/>
-    <div className="p-6 m-4 bg-red-600 rounded-lg shadow-md">
-      <h1 className="text-2xl font-bold text-gray-800">beauty Saloon</h1>
-      <p className="text-gray-600">This is a sample React app using Tailwind CSS.</p>
-    </div>
-    </>
+        </>
   );
 }
 
--- src/component/navbar/index.tsx
+++ src/component/navbar/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiFillInstagram,
+  AiFillFacebook,
+  AiFillYoutube,
+} from "react-icons/ai";
 
 interface IMenuList {
   name: "string";
@@ -20,19 +26,24 @@ const Navbar: React.FC = () => {
 
   return (
     <>
-      <nav className="flex w-full h-12 justify-around items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-bold text-2xl">
+      <nav className="flex w-full h-12 justify-around items-center bg-transparent text-zinc-700 text-xl border-solid border-b-zinc-700 border-b-[1px] font-semibold">
         <h2>Beauty Saloon</h2>
         <div className="sm:hidden text-2xl cursor-pointer" onClick={toggle}>
           {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}{" "}
         </div>
         <ul
-          className={`${isOpen ? "flex" : "hidden"} sm:flex flex-col sm:flex-row sm:w-[30%] w-full h-max justify-center items-center sm:justify-around gap-x-8 sm:gap-x-2.5 top-12 absolute sm:static right-0 text-black sm:text-white`}
+          className={`${isOpen ? "flex" : "hidden"} sm:flex flex-col sm:flex-row sm:w-[35%] w-full h-max justify-center items-center sm:justify-around gap-x-8 top-12 absolute sm:static right-0 text-black sm:text-gray-600 bg-white bg-opacity-10`}
         >
           {menu.map(({ name, link }: IMenuList) => (
             <>
               <div className="">{name}</div>
             </>
           ))}
+          <div className="flex flex-row justify-center items-center gap-4 sm:gap-5 py-2">
+            <AiFillFacebook className="text-2xl" />
+            <AiFillInstagram className="text-2xl" />
+            <AiFillYoutube className="text-2xl" />
+          </div>
         </ul>
       </nav>
     </>

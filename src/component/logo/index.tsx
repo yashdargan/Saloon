7c21758d938d3a19950e75168fc7b1c1a71bb420
yashdargan <yashrajdargan@gmail.com>
2024-09-18 12:11:03 +0530

    Feat: Logo

---
+++ src/component/logo/index.tsx
@@ -0,0 +1,9 @@
+const Logo:React.FC = () => {
+return(
+<div className="flex flex-col w-full justify-center items-center mt-8 py-6">
+      <h1 className="font-extrabold text-5xl">Beauty Saloon</h1>
+      <p className="px-3 italic font-thin text-lg">from hair styling to makeup, we help you achieve the perfect bridal look to make you feel radiant and beautiful as you walk down the aisle.</p>
+</div>
+  )
+}
+export default Logo
